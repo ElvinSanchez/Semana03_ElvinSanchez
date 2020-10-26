55e346f7ad8c4752f5d5fb94f82c9bf5465baa40
Elvin Jesús Sánchez <laden1786@ymail.com>
2020-10-26 02:28:57 -0500

    Agregar archivos de proyecto.

---
+++ Semana03_ElvinSanchez/Semana03_ElvinSanchez/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana03_ElvinSanchez
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrirVista2_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+
+            //consulta bdd usuario pass
+            await Navigation.PushAsync(new Ventana2(user, pass));
+        }
+    }
+}
---
+++ Semana03_ElvinSanchez/Semana03_ElvinSanchez/Ventana2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana03_ElvinSanchez
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ventana2 : ContentPage
+    {
+        public Ventana2(string user, string pass)
+        {
+            InitializeComponent();
+            string usuario = lblUser.Text;
+            lblUser.Text = usuario + user;
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
